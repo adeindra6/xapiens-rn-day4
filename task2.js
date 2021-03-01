d13b9a5d66ace01a892418153bb53708eae9ea2b
Ade Indra Pratama <adeprathama@gmail.com>
2021-03-01 17:27:26 +0700

    Fix task day 4

--- task2.js
+++ task2.js
@@ -3,19 +3,20 @@ const axios = require('axios');
 const url_posts = 'https://jsonplaceholder.typicode.com/posts';
 const url_users = 'https://jsonplaceholder.typicode.com/users';
 
-async function getPost() {
-	await axios.get(url_posts)
-		.then(response => {
-			console.log(response.data)
-		});
-	getUser();
-}
+const getUser = async () => await axios.get(url_users).then((res) => res.data);
 
-async function getUser() {
-	await axios.get(url_users)
-		.then(response => {
-			console.log(response.data)
-		});
-}
+const getPost = async () => await axios.get(url_posts).then((res) => res.data);
 
-getPost();
+const main = async () => {
+	const users = await getUser();
+  	const post = await getPost();
+  	const newPost = post.map((e) => {
+		const user = users.find((u) => u.id === e.userId);
+		e.username = user.username;
+		return e;
+  	});
+
+  	console.log({ newPost });
+};
+
+main();
--- task4.js
+++ task4.js
@@ -6,33 +6,72 @@ async function getEmployee() {
 	await axios.get(url)
 		.then(response => { 
 			this.employee = response.data; 
-			for(let i=0; i<response.data.length; i++) {
-				if(response.data[i].salary > 15000000) {
-					console.log("Salary more than 15 million : " + response.data[i].username);
-				}
 
-				for(let j=0; j<response.data[i].addresses.length; j++) {
-					if(response.data[i].addresses[j].city === 'DKI Jakarta') {
-						console.log("Employee live in Jakarta : " + response.data[i].username);
-					}
+			// Salary more than 15 million
+			let data1 = response.data.filter(function(s) {
+				return s.salary > 15000000;
+			});
+			if(data1 != "") {
+				for(let i=0; i<data1.length; i++) {
+					console.log("Salary more than 15 million : " + data1[i].username);
 				}
+			}
+			else {
+				console.log("No one have salary more than 15 million");
+			}
 
-				if(response.data[i].birthday.substring(5, 7) === '03') {
-					console.log("Birthday in march : " + response.data[i].username);
+			// Employee live in Jakarta
+			let data2 = response.data.filter(function(s) {
+				for(let i=0; i<s.addresses.length; i++) {
+					return s.addresses[i].city === "DKI Jakarta";
 				}
+			});
+			if(data2 != "") {
+				for(let i=0; i<data2.length; i++) {
+					console.log("Employee live in Jakarta : " + data2[i].username);
+				}
+			}
+			else {
+				console.log("No employee live in Jakarta");
+			}
 
-				if(response.data[i].department.name === 'Research and development') {
-					console.log("Research and development employee : " + response.data[i].username);
+			// Birthday in march
+			let data3 = response.data.filter(function(s) {
+				return s.birthday.substring(5, 7) === '03';
+			});
+			if(data3 != "") {
+				for(let i=0; i<data3.length; i++) {
+					console.log("Birthday in march : " + data3[i].username);
 				}
+			}
+			else {
+				console.log("No one birthday in march");
+			}
 
-				let absence = true;
-				for(let j=0; j<response.data[i].presence_list.length; j++) {					
-					if(response.data[i].presence_list[j].substring(5, 7) === '10') {
-						absence = false;
-					}
+			// Research and development employee
+			let data4 = response.data.filter(function(s) {
+				return s.department.name === "Research and development";
+			});
+			if(data4 != "") {
+				for(let i=0; i<data4.length; i++) {
+					console.log("Research and development employee : " + data4[i].username);
 				}
-				if(absence) {
-					console.log("Employee who absence in October 2019 : " + response.data[i].username);
+			}
+			else {
+				console.log("No one research and development employee");
+			}
+
+			// How many absence in October 2019			
+			let data5 = response.data.map(e => {
+				let m = e.presence_list.filter((s) => {
+					return s.slice(5, 7) === "10";
+				});
+				let count = m.length;
+				return count;
+			});
+			if(data5.length > 0) {
+				for(let i=0; i<data5.length; i++) {
+					console.log("How many absence in October 2019 for " + response.data[i].username + " : " + data5[i]);
 				}
 			}
 		});
